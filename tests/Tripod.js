11ee2495acff59f49f0fd6bb2274cb0b2da6445a
Guy John <guy@rumblesan.com>
2017-01-01 22:27:44 +0000

    Add test for area

--- tests/Tripod.js
+++ tests/Tripod.js
@@ -16,8 +16,19 @@ describe('Tripod', () => {
     const expected = new Victor(2, 2);
     const result = Tripod.centre(t);
 
-    assert.equal(result.x, expected.x, 'X values should match');
-    assert.equal(result.y, expected.y, 'Y values should match');
+    assert.equal(result.x, expected.x, `X value should be ${expected.x}`);
+    assert.equal(result.y, expected.y, `Y value should be ${expected.y}`);
   });
 
+  it('calculates the area', () => {
+    const t = Tripod.create(
+      new Victor(1, 1),
+      new Victor(1, 4),
+      new Victor(4, 1)
+    );
+    const expected = 4.5;
+    const result = Tripod.area(t);
+
+    assert.equal(result, expected, `Area should be ${expected}`);
+  });
 });
