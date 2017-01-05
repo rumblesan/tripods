a40324a5b03ab497822a4e2287b9d6ef37d5848b
Guy John <guy@rumblesan.com>
2017-01-05 14:29:23 +0000

    Add getClosestFood function to world

--- src/World.js
+++ src/World.js
@@ -25,3 +25,8 @@ export const eatFood = (world, food) => {
     world.food.splice(idx, 1);
   }
 };
+
+export const closestFood = (world, position) => {
+  if (_.isEmpty(world.food)) return null;
+  return _.min(world.food, (f) => position.distance(f));
+};
